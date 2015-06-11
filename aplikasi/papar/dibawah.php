76b0bb23a2261a46afdb8617196076fa476a92a8
Amin PO Muar <helmi007@yahoo.com>
2015-06-11 15:44:15 +0800

    tambah menu bawah

---
+++ aplikasi/papar/dibawah.php
@@ -0,0 +1,21 @@
+	</div>
+	<!-- end rangka bawah -->
+</div><!--end container-->
+
+<!-- Footer
+================================================== -->
+<footer class="footer">
+	<div class="container">
+		<span class="glyphicon glyphicon-earphone"></span>+062820080
+		<span class="label label-info">
+		&copy; Hak Cipta Terperihara 2014. Theme <?php 
+$theme = ($pilih==null) ? 'Asal Bootstrap Twitter' : $pilih;
+echo $theme = (isset($theme)) ? $theme : null;
+?>	
+		</span>
+	</div>
+</footer>
+<?php require 'jquery.php'; ?>
+
+</body>
+</html>
