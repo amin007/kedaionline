16da6155ed97c553a787a4415cdebafa1331d5bb
Amin PO Muar <helmi007@yahoo.com>
2015-06-11 15:42:22 +0800

    papar untuk login sahaja

--- aplikasi/papar/index/index.php
+++ aplikasi/papar/index/index.php
@@ -1,7 +1,4 @@
 <?php
-//echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">';
-//echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">';
-//echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">';
 
 $kotak[0]='login_fancybox.txt'; 
 $kotak[1]='login_colorbox.txt'; 
@@ -9,23 +6,13 @@ $login=$kotak[rand(0,1)];
 
 $isi=$this->isi;
 
-include 'aset/am/' . $login;
+//include 'aset/am/' . $login;
 $lokasi = 'http://' . $_SERVER['SERVER_NAME'] . '/private_html/bg/kakitangan/';
 ?>
 <!-- ----------------------------------------------------------------------------------- -->
 <table border="0" align="center">
 <tr><td align="center" valign="top"  colspan=2>
-	<a style="font-size: 20pt; text-decoration: overline underline; 
-	background-color: #000000; color:#ffffff">Untuk <?php echo Tajuk_Muka_Surat ?></a>
-	</td></tr>
-<tr><td width="1000" align="center" valign="top">
-	<a style="font-size: 20pt; text-decoration: overline underline; 
-	background-color: #000000; color:#ffffff" class="zoom" 
-	title="Apa Kabar Pentadbir" href="index/login/admin">Pentadbir</a>
-	</td></tr>
-<tr><td align="center" valign="top">
-	<a style="font-size: 15pt; text-decoration: overline underline; 
-	background-color: #000000; color:#ffffff">FE</a><?php 
+<?php 
 unset($pegawai);
 $isi=null;
 $pegawai = senarai_kakitangan();
@@ -38,7 +25,7 @@ foreach ($pegawai as $key => $nama)
 	ucwords($nama) . '" ' .	'href="index/' . $namaFE . '">' .
 	"\n\t" . '<img src="' . $lokasi . $nama . '.jpg"></a>';
 	
-	$isi.=($key==4)? "<br>\n":"\n";
+	$isi.=($key==3)? "<br>\n":"\n";
 }
 	echo $isi;
 ?>	
