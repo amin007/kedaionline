629861f7f0846b5e5ccdc5c8c69867063df16e51
Amin PO Muar <helmi007@yahoo.com>
2015-06-11 13:14:44 +0800

    seting untuk twitter bootstrap

--- aplikasi/kawal/ruangtamu.php
+++ aplikasi/kawal/ruangtamu.php
@@ -36,7 +36,8 @@ class Ruangtamu extends Kawal
 		$this->papar->pegawai = senarai_kakitangan();
 		$this->papar->tajuk = 'Ruangtamu';
 		// pergi papar kandungan
-		$this->papar->baca('ruangtamu/index', 0);
+		$this->papar->baca('ruangtamu/index'); // untuk twitter bootstrap
+		//$this->papar->baca('ruangtamu/index', 1); // tanpa twitter bootstrap
 		//$this->papar->baca('ruangtamu/index', 'mobile');
 		//$this->papar->baca('mobile/mobile');
 	}
