fb92e654ca9b4edc4b035e48825b0dbd3a22f43c
Amin PO Muar <helmi007@yahoo.com>
2015-06-11 14:31:44 +0800

    ringkaskan kod

--- aplikasi/papar/index/index.php
+++ aplikasi/papar/index/index.php
@@ -1,6 +1,6 @@
 <?php
 //echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">';
-echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">';
+//echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">';
 //echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">';
 
 $kotak[0]='login_fancybox.txt'; 
@@ -8,19 +8,10 @@ $kotak[1]='login_colorbox.txt';
 $login=$kotak[rand(0,1)];
 
 $isi=$this->isi;
-?>
-<html><head><title><?php echo Tajuk_Muka_Surat ?></title>
-<link rel="stylesheet" href="<?php echo JS ?>public/css/gambar_head.css" />
-<?php
-//include '../../js/public/css/' . $login;
+
 include 'aset/am/' . $login;
 $lokasi = 'http://' . $_SERVER['SERVER_NAME'] . '/private_html/bg/kakitangan/';
 ?>
-</head>
-<body background="<?php echo GAMBAR ?>">
-<div id="content">
-<table border="0" height="90%" width="90%">
-<tr><td align="center" valign="middle"> 
 <!-- ----------------------------------------------------------------------------------- -->
 <table border="0" align="center">
 <tr><td align="center" valign="top"  colspan=2>
@@ -37,8 +28,7 @@ $lokasi = 'http://' . $_SERVER['SERVER_NAME'] . '/private_html/bg/kakitangan/';
 	background-color: #000000; color:#ffffff">FE</a><?php 
 unset($pegawai);
 $isi=null;
-$pegawai=array('adam','amin','ariff','azim','fendi','irwan','khairi',
-'norita','musa','mustaffa','shukor','suhaida');
+$pegawai = senarai_kakitangan();
 
 foreach ($pegawai as $key => $nama)
 {	
@@ -53,31 +43,5 @@ foreach ($pegawai as $key => $nama)
 	echo $isi;
 ?>	
 	</td></tr>
-<tr><td align="center" valign="top"  colspan=2>
-	<a style="font-size: 15pt; text-decoration: overline underline; 
-	background-color: #000000; color:#ffffff">KUP</a><?php 
-unset($kup);
-$isi=null;
-$kup=array('murad','sujana');
-foreach ($kup as $key => $nama)
-{	
-	$isi.="\n\t" . '<a class="zoom" title="Assalamualaikum ' .
-	ucwords($nama) . '" ' .	'href="index/login/' . $nama . '">' .
-	"\n\t" . '<img src="' . $lokasi . $nama . '.jpg"></a>';
-	
-	$isi.=($key==5)? "<br>\n":"\n";
-}
-	echo $isi;?><br>
-	<a style="font-size: 15pt; text-decoration: overline underline; 
-	background-color: #000000; color:#ffffff">PEGAWAI</a>
-	<a class="zoom" title="Apa Kabar En Abdul Razak" href="index/login_automatik/razak"
-	style="font-size: 20pt; background-color: #ffff00; color:#ff0000">Razak</a>
-	<a class="zoom" title="Apa Kabar En Azhari" href="index/login_automatik/azhari"
-	style="font-size: 20pt; background-color: #ffff00; color:#ff0000">Azhari</a>
-	</td></tr>
 </table>
 <!-- ----------------------------------------------------------------------------------- -->
-</td></tr></table>
-</div>
-</body>
-</html>
