e2f3cc521a5cb445096f694e589694f0d0d95ad3
Amin PO Muar <helmi007@yahoo.com>
2015-06-11 13:14:20 +0800

    seting untuk css twitter bootstrap

--- aplikasi/kawal/index.php
+++ aplikasi/kawal/index.php
@@ -45,8 +45,8 @@ class Index extends Kawal
 		$this->papar->server = $_SERVER['SERVER_NAME'];
 
 		// pergi papar kandungan
-		//$this->papar->baca('index/index', 0 ); // untuk css
-		//$this->papar->baca('index/index', 1 ); // tanpa css
+		//$this->papar->baca('index/index'); // untuk twitter bootstrap
+		//$this->papar->baca('index/index', 1 ); // tanpa twitter bootstrap
 		$this->papar->baca('mobile/login', 'mobile');
 	}
 	
