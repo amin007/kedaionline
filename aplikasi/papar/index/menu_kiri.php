97b0165dbb116ee3c4e8a00ff8652bd3bfa271ac
Amin PO Muar <helmi007@yahoo.com>
2015-06-17 13:42:39 +0800

    ubah lokasi link

--- aplikasi/papar/index/menu_kiri.php
+++ aplikasi/papar/index/menu_kiri.php
@@ -8,25 +8,26 @@
 				</div>
 			</div>
 			<!-- end iklan -->
-			
+<?php $url1 = URL . 'kategori/';	?>
 			<!-- kategori makanan-->
 			<div class="col-xs-12 col-sm-12">
 				<div class="list-group none1">
 					<p href="#" class="list-group-item active"> Kategori Makanan </p>
-					<a href="kategori/ringan" class="list-group-item">Makanan Ringan <span class="badge">2</span></a>
-					<a href="kategori/kuih" class="list-group-item">Kuih Tradisional <span class="badge">2</span></a>
-					<a href="kategori/sos" class="list-group-item">Sos Pencicah <span class="badge">2</span></a>
-					<a href="kategori/pes" class="list-group-item">Pes Masakan <span class="badge">2</span></a>
+					<a href="<?php echo $url1 ?>makanan/ringan" class="list-group-item">Makanan Ringan <span class="badge">2</span></a>
+					<a href="<?php echo $url1 ?>kuih/tradisional" class="list-group-item">Kuih Tradisional <span class="badge">2</span></a>
+					<a href="<?php echo $url1 ?>sos/pencicah" class="list-group-item">Sos Pencicah <span class="badge">2</span></a>
+					<a href="<?php echo $url1 ?>pes/masakan" class="list-group-item">Pes Masakan <span class="badge">2</span></a>
 				</div>
 			</div>
 			<!-- end kategori makanan-->
 
 			<!--box info-->
+<?php $url2 = URL . 'infomasi/'; ?>
 			<div class="col-lg-12 col-md-12">
 				<div class="list-group none1">
 					<p href="#" class="list-group-item active"> Infomasi </p>
-					<a href="infomasi/" class="list-group-item">Tentang Kami</a>
-					<a href="infomasi/hubungi/kami" class="list-group-item">Hubungi Kami</a>
+					<a href="<?php echo $url2 ?>" class="list-group-item">Tentang Kami</a>
+					<a href="<?php echo $url2 ?>hubungi/kami" class="list-group-item">Hubungi Kami</a>
 				</div>
 			</div>
 			<!--end box info-->
@@ -35,11 +36,11 @@
 			<div class="col-lg-12 col-md-12">
 				<div class="list-group none1">
 					<p href="#" class="list-group-item active"> Carian Produk </p>
-					<form role="search">
+					<form role="search" method="POST" action="<?php echo $url1 ?>carian">
 					<div class="form-group">
-					<input type="text" class="form-control" placeholder="Carian">
+					<input type="text" name="cariapa" class="form-control" placeholder="Carian">
 					</div>
-					<button type="submit" class="btn btn-default">Cari</button>
+					<input type="submit" value="Cari" class="btn btn-default">
 					</form>
 				</div>
 			</div>
@@ -47,15 +48,15 @@
 <?php 
 $dataURL = dpt_url();
 //echo '<pre>'; echo '<br>$dataURL:<br>'; print_r($dataURL); echo '</pre>';
-$classKhas = array('infomasi');
+//$classKhas = array('infomasi','kategori');
+$classKhas = array('index');
 if ( isset($dataURL[0]) && ( in_array($dataURL[0],$classKhas) )) :
-	echo '';
-else:
-?>			
-			<!--special offer-->
+?>			<!--special offer-->
 <?php include 'tawaran_istimewa.php'; ?>
 			<!--end special offer-->
 <?php
+else:
+	echo '';
 endif;
 ?>
 <!-- end senarai yang ada pada menu kiri /////////////////////////////////////////////////////////// -->
