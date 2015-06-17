c480dfd5d1a0dc1e4236398562c33bfdb816b0f9
Amin PO Muar <helmi007@yahoo.com>
2015-06-17 13:38:59 +0800

    1. ubah level ikut dalam jadual pangkalana data 2. guna if else untuk menu sebelum login dan menu selepas login

--- aplikasi/papar/menu_atas.php
+++ aplikasi/papar/menu_atas.php
@@ -1,25 +1,16 @@
 <?php 
-Sesi::init(); // mulakan sesi
+Sesi::init(); # mulakan sesi
 //echo '<pre>$_SESSION:', print_r($_SESSION, 1) . '</pre><br>';
-/*
-$_SESSION:Array
-(
-    [namaPegawai] => musa
-    [levelPegawai] => fe
-    [loggedIn] => 1
-)
-*/
-// set pembolehubah
-$pengguna = Sesi::get('namaPegawai');
-$level = Sesi::get('levelPegawai');
-$pegawai = null; //$this->pegawai;
+# set pembolehubah
+$pengguna = Sesi::get('namaPengguna');
+$level = Sesi::get('levelPengguna');
 
-$senaraiPengguna = array('fe','pegawai');
-$senaraiPentadbir = array('kawal','admin');
-if (in_array(Sesi::get('levelPegawai'), $senaraiPentadbir)) 
-	$paras = '' . Sesi::get('levelPegawai');
+$senaraiPengguna = array('ahli','jurujual');
+$senaraiPentadbir = array('pentadbir');
+if (in_array(Sesi::get('levelPengguna'), $senaraiPentadbir)) 
+	$paras = '' . Sesi::get('levelPengguna');
 elseif (in_array($level, $senaraiPengguna)) 
-	$paras = '' . Sesi::get('levelPegawai');
+	$paras = '' . Sesi::get('levelPengguna');
 else
 	$paras = null;
 $url = URL;
@@ -45,10 +36,16 @@ $url = URL;
 	</div>
 	<div class="collapse navbar-collapse">
 		<ul class="nav navbar-nav navbar-right">
+<?php if ($paras == null):?>
 		<li><a href="#" class="btn btn-success btn-sm"
 		data-toggle="modal" data-target="#daftarAkaun">Daftar Akaun</a></li>
 		<li><a href="#" class="btn btn-navbar-custom"
 		data-toggle="modal" data-target="#logMasuk">Log Masuk</a></li>
+<?php else: ?>
+		<li><a href="#" class="btn btn-navbar-custom"><?php echo $paras ?></a></li>
+		<li><a href="<?php echo URL . 'ruangtamu/logout';
+		?>" class="btn btn-navbar-custom">Log Keluar</a></li>
+<?php endif; ?>
 		</ul>
 	</div><!-- /.navbar-collapse -->
 	</nav>
@@ -58,6 +55,11 @@ $url = URL;
 	<div class="row" style=" margin-top:-10px">
 		<!-- menu kiri -->
 		<div class="col-md-3">
-<?php include 'index/menu_kiri.php'; ?>
+<?php 
+if ($paras == null):
+	include 'index/menu_kiri.php'; 
+else:
+endif;	
+?>
 		</div>
 		<!-- end menu kiri -->
