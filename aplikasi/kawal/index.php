c7775b6f54f2d49bebfd427e893c7fed59729227
Amin PO Muar <helmi007@yahoo.com>
2015-06-21 15:59:38 +0800

    1. ubah tatasusunan $this->papar->js dan $this->papar->css 2. ringkaskan kod tatasusunan $this->papar-> tawaran, terkini, terlaris, daftarAkaun, loginMasuk, ingatPassword

--- aplikasi/kawal/index.php
+++ aplikasi/kawal/index.php
@@ -6,49 +6,70 @@ class Index extends Kawal
 	function __construct() 
 	{
 		parent::__construct();
-        Kebenaran::kawalMasuk();
+		Kebenaran::kawalMasuk();
 
-        $this->papar->js = array(
-            //'bootstrap.js',
-            'bootstrap-datepicker.js',
-            'bootstrap-datepicker.ms.js',
-            //'bootstrap-editable.min.js',
-			//'bootstrap-lightbox.js'
-			'../../js/function.js',
-			'../../js/pwd.js',
-			'../../lightbox/js/lightbox-2.6.min.js',);
-        $this->papar->css = array(
-			'bootstrap-datepicker.css',
-			'bootstrap-editable.css',
-			'../../css/navbar.css',
-			'../../css/style.css',
-			'../../lightbox/css/lightbox.css',);
-		
+		$this->papar->js = array(
+			//'bootstrap-datepicker.js','bootstrap-datepicker.ms.js','bootstrap-editable.min.js','bootstrap-lightbox.js'
+			'bootstrap-3.3.4-dist/js/bootstrap.js', # untuk muka depan sahaja
+			'js/function.js','js/pwd.js','lightbox/js/lightbox-2.6.min.js',# untuk muka depan sahaja
+			);
+		$this->papar->css = array(
+			//'bootstrap-datepicker.css','bootstrap-editable.css',
+			'bootstrap-3.3.4-dist/css/bootstrap.css', # untuk muka depan sahaja
+			'css/navbar.css','css/style.css','lightbox/css/lightbox.css',# untuk muka depan sahaja
+			);		
 	}
 	
 	function index() 
 	{
-		// masukkan tatasususunan dari Tanya
-		$this->papar->tawaran = $this->tanya->tawaran(
-			$myTable = null, $medan = null, 
-			$carian = null, $susun = null);
-		$this->papar->terkini = $this->tanya->terkini(
-			$myTable = null, $medan = null, 
-			$carian = null, $susun = null);
-		$this->papar->terlaris = $this->tanya->terlaris(
-			$myTable = null, $medan = null, 
-			$carian = null, $susun = null);
-		// semak tatasusunan $this->papar->produk
-		//echo '<pre>$this->papar->tawaran->'; print_r($this->papar->tawaran) . '</pre>';
-		//echo '<pre>$this->papar->terkini->'; print_r($this->papar->terkini) . '</pre>';
-		//echo '<pre>$this->papar->terlaris->'; print_r($this->papar->terlaris) . '</pre>';
-		// pergi papar kandungan
+		# masukkan tatasususunan dari Tanya
+		$this->papar->tawaran = $this->tanya->tawaran();
+		$this->papar->terkini = $this->tanya->terkini();
+		$this->papar->terlaris = $this->tanya->terlaris();
+		$this->papar->daftarAkaun = $this->tanya->daftar();
+		$this->papar->loginMasuk = $this->tanya->login();
+		$this->papar->ingatPassword = $this->tanya->ingat();
+
+		# pergi papar kandungan
 		$this->papar->baca('index/gambar3'); // untuk twitter bootstrap
 		//$this->papar->baca('index/index'); // untuk twitter bootstrap
 		//$this->papar->baca('index/index', 1 ); // tanpa twitter bootstrap
 		//$this->papar->baca('index/index', 'mobile');
 	}
+
+	function daftarahli() 
+	{
+		# masukkan tatasususunan dari Tanya
+		$this->papar->tawaran = $this->tanya->tawaran();
+		$this->papar->terkini = $this->tanya->terkini();
+		$this->papar->terlaris = $this->tanya->terlaris();
+		$this->papar->daftarAkaun = $this->tanya->daftar();
+		$this->papar->loginMasuk = $this->tanya->login();
+		$this->papar->ingatPassword = $this->tanya->ingat();
+
+		# pergi papar kandungan
+		$this->papar->baca('index/daftarahli'); // untuk twitter bootstrap
+		//$this->papar->baca('index/index'); // untuk twitter bootstrap
+		//$this->papar->baca('index/index', 1 ); // tanpa twitter bootstrap
+		//$this->papar->baca('index/index', 'mobile');
+	}
 	
+	function tukarpassword() 
+	{
+		# masukkan tatasususunan dari Tanya
+		$this->papar->tawaran = $this->tanya->tawaran();
+		$this->papar->terkini = $this->tanya->terkini();
+		$this->papar->terlaris = $this->tanya->terlaris();
+		$this->papar->daftarAkaun = $this->tanya->daftar();
+		$this->papar->loginMasuk = $this->tanya->login();
+		$this->papar->ingatPassword = $this->tanya->ingat();
+
+		# pergi papar kandungan
+		$this->papar->baca('index/tukarpassword'); // untuk twitter bootstrap
+		//$this->papar->baca('index/index'); // untuk twitter bootstrap
+		//$this->papar->baca('index/index', 1 ); // tanpa twitter bootstrap
+		//$this->papar->baca('index/index', 'mobile');
+	}
 	function login($user) 
 	{
 		$this->papar->nama=$user; # dapatkan nama pengguna
@@ -69,7 +90,6 @@ class Index extends Kawal
 	{
 		// set latarbelakang
 		$this->papar->gambar=gambar_latarbelakang('../../');
-		$this->papar->isi='';
 		$this->papar->senaraiIP = dpt_ip(); # dapatkan senarai IP yang dibenarkan
 		$this->papar->ip = $ip = $_SERVER['REMOTE_ADDR'];
 		$this->papar->ip2 = substr($ip,0,10);
