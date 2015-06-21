04d8dcc4749622bffff1c0a2426f1b8d9ca91d41
Amin PO Muar <helmi007@yahoo.com>
2015-06-21 16:25:12 +0800

    1. ringkaskan kod level pengguna 2. alihkan kod css berasaskan $paras

--- aplikasi/papar/menu_atas.php
+++ aplikasi/papar/menu_atas.php
@@ -4,18 +4,16 @@ Sesi::init(); # mulakan sesi
 # set pembolehubah
 $pengguna = Sesi::get('namaPengguna');
 $level = Sesi::get('levelPengguna');
-
-$senaraiPengguna = array('ahli','jurujual');
-$senaraiPentadbir = array('pentadbir');
-if (in_array(Sesi::get('levelPengguna'), $senaraiPentadbir)) 
-	$paras = '' . Sesi::get('levelPengguna');
-elseif (in_array($level, $senaraiPengguna)) 
-	$paras = '' . Sesi::get('levelPengguna');
-else
-	$paras = null;
+# semak
+if (Sesi::get('levelPengguna')=='pentadbir') 	$paras = Sesi::get('levelPengguna');
+elseif (Sesi::get('levelPengguna')=='jurujual')	$paras = Sesi::get('levelPengguna');
+elseif (Sesi::get('levelPengguna')=='ahli')		$paras = Sesi::get('levelPengguna');
+else $paras = null;
+/*<img alt="Brand" src="<?php echo $url ?>images/back/chart.fw.png" 
+width="30" height="30" alt="chart">*/
 $url = URL;
-?>
-<div class="container" style="margin-top:-20px;border: 2px solid #dfdfd0; border-radius:2px">
+if ($paras == null):?>
+<div class="container" style="margin-top:10px;border:2px solid #dfdfd0; border-radius:2px">
 	<!-- rangka atas -->
 	<nav class="navbar navbar-custom">
 	<div class="navbar-header">
@@ -25,41 +23,21 @@ $url = URL;
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
 		</button>
-		<a class="navbar-brand" href="#" data-toggle="modal" data-target="#myCart">
-			<span><img alt="Brand" src="<?php echo $url ?>images/back/chart.fw.png" width="30" height="30" 
-			alt="chart">Shopping Cart</span>
-		</a>
 		<a class="navbar-brand" href="<?php echo $url ?>">
-			E-SME Food: A web based application for SME food for online shopping
-		</a>
-		
+		E-SME Food: A web based application for SME food for online shopping
+		</a>		
 	</div>
 	<div class="collapse navbar-collapse">
-		<ul class="nav navbar-nav navbar-right">
-<?php if ($paras == null):?>
-		<li><a href="#" class="btn btn-success btn-sm"
-		data-toggle="modal" data-target="#daftarAkaun">Daftar Akaun</a></li>
-		<li><a href="#" class="btn btn-navbar-custom"
-		data-toggle="modal" data-target="#logMasuk">Log Masuk</a></li>
-<?php else: ?>
-		<li><a href="#" class="btn btn-navbar-custom"><?php echo $paras ?></a></li>
-		<li><a href="<?php echo URL . 'ruangtamu/logout';
-		?>" class="btn btn-navbar-custom">Log Keluar</a></li>
-<?php endif; ?>
-		</ul>
+<?php include 'menubar_atas.php'; ?>
 	</div><!-- /.navbar-collapse -->
-	</nav>
-	<!-- end rangka atas -->
-	
+	</nav><!-- end rangka atas -->
+
 	<!-- rangka bawah -->
 	<div class="row" style=" margin-top:-10px">
 		<!-- menu kiri -->
 		<div class="col-md-3">
-<?php 
-if ($paras == null):
-	include 'index/menu_kiri.php'; 
-else:
-endif;	
-?>
+<?php include 'index/menu_kiri.php'; ?>
 		</div>
 		<!-- end menu kiri -->
+<?php else: endif; echo "\n";
+?>
