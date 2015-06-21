ec76392e313db315b8da48814cac2a47e79bed5a
Amin PO Muar <helmi007@yahoo.com>
2015-06-21 16:04:42 +0800

    ubah tatasusunan js dan css

--- aplikasi/kawal/kategori.php
+++ aplikasi/kawal/kategori.php
@@ -8,22 +8,16 @@ class Kategori extends Kawal
 		parent::__construct();
         Kebenaran::kawalMasuk();
 
-        $this->papar->js = array(
-            //'bootstrap.js',
-            'bootstrap-datepicker.js',
-            'bootstrap-datepicker.ms.js',
-            //'bootstrap-editable.min.js',
-			//'bootstrap-lightbox.js'
-			'../../js/function.js',
-			'../../js/pwd.js',
-			'../../lightbox/js/lightbox-2.6.min.js',);
-        $this->papar->css = array(
-			'bootstrap-datepicker.css',
-			'bootstrap-editable.css',
-			'../../css/navbar.css',
-			'../../css/style.css',
-			'../../lightbox/css/lightbox.css',);
-		
+		$this->papar->js = array(
+			//'bootstrap-datepicker.js','bootstrap-datepicker.ms.js','bootstrap-editable.min.js','bootstrap-lightbox.js'
+			'bootstrap-3.3.4-dist/js/bootstrap.js', # untuk muka depan sahaja
+			'js/function.js','js/pwd.js','lightbox/js/lightbox-2.6.min.js',# untuk muka depan sahaja
+			);
+		$this->papar->css = array(
+			//'bootstrap-datepicker.css','bootstrap-editable.css',
+			'bootstrap-3.3.4-dist/css/bootstrap.css', # untuk muka depan sahaja
+			'css/navbar.css','css/style.css','lightbox/css/lightbox.css',# untuk muka depan sahaja
+			);
 	}
 	
 	function index() 
