c826bdfbc629261cd3e9aa9e1d3080aee24663a1
Amin PO Muar <helmi007@yahoo.com>
2015-06-21 16:23:26 +0800

    1. tambah icon. 2. ringkaskan kod untuk proses $this->css

--- aplikasi/papar/diatas.php
+++ aplikasi/papar/diatas.php
@@ -4,32 +4,33 @@
 <meta charset="utf-8">
 <title><?php
 /* style="background: url('<?php echo GAMBAR ?>') no-repeat center center fixed;background-size: cover;"*/
-// papar title
+# papar title
 echo Tajuk_Muka_Surat;
-
 $dpt_url = dpt_url();
 echo (empty($dpt_url[2])) ? null : '[' . $_GET['url'] . ']';
+# untuk set icon bergambar
+$troli = '<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>';
+$semak = '<span class="glyphicon glyphicon-check" aria-hidden="true"></span>';
+$daftar = '<span class="glyphicon glyphicon-briefcase" aria-hidden="true"></span>';
+$orang = '<span class="glyphicon glyphicon-user" aria-hidden="true"></span>';
+$surat = '<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>';
+$suistutup = '<span class="glyphicon glyphicon-off" aria-hidden="true"></span>';
+$mangga = '<span class="glyphicon glyphicon-lock" aria-hidden="true"></span>';
+$logmasuk = '<span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>';
+$logkeluar = '<span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>';
+$icon['Barcode'] = '<span class="glyphicon glyphicon-barcode" aria-hidden="true"></span>';
+$icon['Filter'] = '<span class="glyphicon glyphicon-filter" aria-hidden="true"></span>';
+$icon['Stats'] = '<span class="glyphicon glyphicon-stats" aria-hidden="true"></span>';
 ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
-$bootstrap = URL . 'aset/bootstrap-3.3.4-dist/';
-$css_url = $bootstrap . 'css/';
-$js_url  = $bootstrap . 'js/';
-$ico_url = $bootstrap . 'img/';
-
-require 'diatas-bootstrap.php';
-
-if (isset($this->css)) 
-{
-	foreach ($this->css as $css)
-	{
-		echo "\n"; // '<link rel="stylesheet" type="text/css" href="' . . $css . '">';
-?><link rel="stylesheet" href="<?php echo $css_url . $css ?>"><?php
-	}
-}
-echo "\n";
-
-?>
+if (isset($this->css)):
+	foreach ($this->css as $css):
+		echo "\n";
+?><link rel="stylesheet" href="<?php echo URL . 'aset/' . $css; ?>"><?php
+	endforeach;
+endif;
+echo "\n"; ?>
 <style type="text/css">
 table.excel {
 	border-style:ridge;
@@ -55,6 +56,3 @@ table.excel tbody td
 </style>
 </head>  
 <body>
-<ol class="breadcrumb">
-	<li></li>
-</ol>
